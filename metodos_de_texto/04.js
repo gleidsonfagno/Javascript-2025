e6ca68e1fd883b8670797be44cf4ce73b6e32f13
gleidson fagno <gleidsonfagner838@gmail.com>
2025-02-25 00:22:02 -0300

    feat: Encontrando um conteudo no texto

---
+++ metodos_de_texto/04.js
@@ -0,0 +1,12 @@
+let texto = "Estuda, aprender, Praticar"
+
+// Separa  strings
+let separate = texto.split(",")
+console.log(separate)
+
+let message = "Estou aprendendo Javascript"
+console.log(message.split(" "))
+
+// unir a String
+let joined =  separate.join(" - ")
+console.log(joined)
---
+++ metodos_de_texto/05.js
@@ -0,0 +1,9 @@
+let messege = "Estou estudando os fundamentos do Javascript em 2025"
+
+//indexOf Obtém a posição da string.
+console.log(messege.indexOf("Javascript"))
+
+// includes verifica se existe a palavra na string.
+console.log(messege.includes("Javascript"))
+
+console.log(messege.includes("Estou estudando os fundamentos"))
