217032e4ba29e1a8f22b9305b6e5818229717287
gleidson fagno <gleidsonfagner838@gmail.com>
2025-02-24 22:25:16 -0300

    feat: metosdos de maiusculo e minusculo

---
+++ metodos_de_texto/01.js
@@ -0,0 +1,9 @@
+let messege = "Estou estudando os fundamentos de Javscript"
+
+console.log(messege)
+
+// maisculo
+console.log(messege.toUpperCase())
+
+// minusculo
+console.log(messege.toLowerCase())
