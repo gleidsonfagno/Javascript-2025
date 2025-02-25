445ccf560c84c5fc7ecc1b3e7bbd86917209a8ec
gleidson fagno <gleidsonfagner838@gmail.com>
2025-02-24 22:30:35 -0300

    feat: comprimento de uma string

--- metodos_de_texto/01.js
+++ metodos_de_texto/01.js
@@ -7,3 +7,17 @@ console.log(messege.toUpperCase())
 
 // minusculo
 console.log(messege.toLowerCase())
+
+// comprimento de uma variavel conta espaços
+console.log(messege.length)
+
+let password = "12345"
+
+if (password.length < 6) {
+    console.log("A  senha deve conter almenos 6 digitos")
+}
+
+// quantos digitos tem um número
+let numero =  1234
+console.log(String(numero).length)
+console.log(numero.toString().length)
