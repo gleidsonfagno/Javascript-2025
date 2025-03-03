c235cee48a37c0f4b5ee36cc9fcc9086fb816fe3
gleidson fagno <gleidsonfagner838@gmail.com>
2025-03-03 11:43:37 -0300

    feat: Método map

---
+++ javascript_moderno/arrays/map.js
@@ -0,0 +1,30 @@
+// Metétodo map() chama  a  função callback recebida por parâmetro para cada elemento do array original, em ordem, e coonstrói um novo com base nos retornos de cada chamada. E no final, devolve o novo array.
+
+const products = [
+    "Teclado",
+    "Notebook",
+    "Mouse",
+    "Monitor",
+]
+
+// percorrendo os itens do array
+products.map((product) => {
+    console.log(product)
+})
+
+// sintaxe reduzida
+// products.map((product) => console.log(product))
+
+// Utilizando o novo  objeto retornado.
+const formatted = products.map((product) => {
+    // return product.toUpperCase()
+
+
+    return {
+        id: Math.random(),
+        description: product,
+    }
+    
+})
+
+console.log(formatted)
