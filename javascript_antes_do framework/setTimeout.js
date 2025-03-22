f9cb46f5f4dd061e6bfd0f0a1e02e1becd709611
gleidson fagno <gleidsonfagner838@gmail.com>
2025-03-22 18:46:46 -0300

    feat: setTimeout

---
+++ javascript_antes_do framework/setTimeout.js
@@ -0,0 +1,8 @@
+// setTumeout() execcuta uma função após um intervalo de tempo especificado.
+
+console.log("antes")
+setTimeout(() => {
+    console.log("Olá, mundo")
+}, 3000); // tempo em milesegundos
+
+
